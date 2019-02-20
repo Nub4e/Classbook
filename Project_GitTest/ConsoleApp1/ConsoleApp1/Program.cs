60f553d61c2cc9a8b11ba42b63604b5b48da6aee
thealmostnooBBg <thealmostnooBBg@abv.bg>
2019-02-20 22:42:11 +0200

    ye

--- Project_GitTest/ConsoleApp1/ConsoleApp1/Program.cs
+++ Project_GitTest/ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("not rly a part of the project");
+            Console.WriteLine("not rly a part of the project + some changes");
         }
     }
 }
